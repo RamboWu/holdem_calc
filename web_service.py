dbb217178e03c035c00772b619a7a2d3b73ce678
Rambo <RamboWu90@gmail.com>
2020-08-11 13:20:15 +0800

    Create web_service.py
    尝试搭入web服务器

---
+++ web_service.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import holdem_calc
+import holdem_argparser
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/calc')
+def calc_holdem():
+    exact = False
+    num = 100000
+    hole_cards_args = ['Kc', 'Kh', 'Ac', 'Ah']
+    board_args = ['5c', '6c', '7c']
+    hole_cards, board = holdem_argparser.parse_cards(hole_cards_args, board_args)
+    holdem_calc.run(hole_cards, num, exact, board, None, True)
+    return 'start World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
